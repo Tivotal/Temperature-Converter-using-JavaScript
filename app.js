1708db242fecd9bbb67c9bd3f7c1139c61c99f9e
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-01-14 08:13:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+/* Created by Tivotal */
+
+let celsiusInput = document.querySelector(".celsius");
+let fahrenheitInput = document.querySelector(".fahrenheit");
+
+window.addEventListener("load", () => celsiusInput.focus());
+
+celsiusInput.addEventListener("input", () => {
+  fahrenheitInput.value = ((celsiusInput.value * 9) / 5 + 32).toFixed(2);
+
+  if (!celsiusInput.value) fahrenheitInput.value = "";
+});
+
+fahrenheitInput.addEventListener("input", () => {
+  celsiusInput.value = (((fahrenheitInput.value - 32) * 5) / 9).toFixed(2);
+
+  if (!fahrenheitInput.value) celsiusInput.value = "";
+});
